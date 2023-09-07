a27a6ace6da08d9786b30736769cdbbb701e9479
chyevus <144251519+chyevus@users.noreply.github.com>
2023-09-07 23:57:59 +0200

    Update chyevus.js

--- chyevus.js
+++ chyevus.js
@@ -1,6 +1,6 @@
 //You are going in the right direction...
 document.getElementById("submitbutton")?.addEventListener("click", function() {
-    console.log('Login in...')
+    //Created by Chyevus...
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     sendCredentials(email,password)
